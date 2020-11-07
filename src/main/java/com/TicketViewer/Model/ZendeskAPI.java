07a1bc3a90c0e3dbf53e18009b42140948419fb3
tmingora <tmingora@enssat.fr>
2020-11-07 15:27:27 +0000

    Initial commit

---
+++ src/main/java/com/TicketViewer/Model/ZendeskAPI.java
@@ -0,0 +1,20 @@
+package com.TicketViewer.Model;
+
+import org.zendesk.client.v2.Zendesk;
+
+public class ZendeskAPI {
+    public static Zendesk instance = null;
+
+    public static Zendesk getInstance(){
+        if (ZendeskAPI.instance == null) {
+            ZendeskAPI.instance = new Zendesk.Builder("https://enssat.zendesk.com")
+                    .setUsername("tmingora@enssat.fr")
+                    .setPassword("123456")
+                    .build();
+            return ZendeskAPI.instance;
+        }
+        else{
+            return ZendeskAPI.getInstance();
+        }
+    }
+}
