daf906383b2e5746a633396c00cee1c43ea15d79
tmingora <tmingora@enssat.fr>
2020-11-08 12:44:21 +0000

    TODOs

--- src/main/java/com/TicketViewer/Controller/TicketListUI.java
+++ src/main/java/com/TicketViewer/Controller/TicketListUI.java
@@ -2,6 +2,7 @@ package com.TicketViewer.Controller;
 
 import org.zendesk.client.v2.model.Ticket;
 
-public class TicketListUI{
+public class TicketListUI{//TODO methods that parse entries
+    //TODO method that retrieves the Tickets and the couont of tickets (so HTTP request + convert into JSON)
 
 }
