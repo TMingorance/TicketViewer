16e4e130dbef621dd32c5a6a8ddf94ae8617b5a5
tmingora <tmingora@enssat.fr>
2020-11-12 19:23:27 +0000

    Tests

--- src/main/java/com/TicketViewer/View/Utilities.java
+++ src/main/java/com/TicketViewer/View/Utilities.java
@@ -1,9 +1,13 @@
 package com.TicketViewer.View;
 
+import java.io.IOException;
+
 public class Utilities {
 
     public static void clearScreen(){
+        /* //doesn't work on windows
         System.out.println("\033[H\033[2J");
         System.out.flush();
+        */
     }
 }
